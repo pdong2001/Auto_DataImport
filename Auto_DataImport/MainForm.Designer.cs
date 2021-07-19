06b5dd06007a6de5abdfe8e9ff5752d265c20021
pdong2001 <79776591+pdong2001@users.noreply.github.com>
2021-07-19 13:31:37 +0700

    Add project files.

---
+++ Auto_DataImport/MainForm.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace Auto_DataImport
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ofdGetExcel = new System.Windows.Forms.OpenFileDialog();
+            this.btnShowFileDialog = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnImportStart = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ofdGetExcel
+            // 
+            this.ofdGetExcel.InitialDirectory = "C: \\";
+            this.ofdGetExcel.Title = "Mở tệp excel";
+            // 
+            // btnShowFileDialog
+            // 
+            this.btnShowFileDialog.AutoSize = true;
+            this.btnShowFileDialog.Location = new System.Drawing.Point(712, 10);
+            this.btnShowFileDialog.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnShowFileDialog.Name = "btnShowFileDialog";
+            this.btnShowFileDialog.Size = new System.Drawing.Size(56, 23);
+            this.btnShowFileDialog.TabIndex = 0;
+            this.btnShowFileDialog.Text = "Browse";
+            this.btnShowFileDialog.UseVisualStyleBackColor = true;
+            this.btnShowFileDialog.Click += new System.EventHandler(this.btnShowFileDialog_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 43);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(857, 312);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.numericUpDown2);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.btnImportStart);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.btnShowFileDialog);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(857, 43);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnImportStart
+            // 
+            this.btnImportStart.AutoSize = true;
+            this.btnImportStart.Location = new System.Drawing.Point(9, 10);
+            this.btnImportStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnImportStart.Name = "btnImportStart";
+            this.btnImportStart.Size = new System.Drawing.Size(56, 23);
+            this.btnImportStart.TabIndex = 2;
+            this.btnImportStart.Text = "Start";
+            this.btnImportStart.UseVisualStyleBackColor = true;
+            this.btnImportStart.Click += new System.EventHandler(this.btnImportStart_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(571, 11);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(153, 13);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(366, 13);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Bắt đầu từ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(313, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Số dòng";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 355);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "MainForm";
+            this.Text = "Auto Data Import";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog ofdGetExcel;
+        private System.Windows.Forms.Button btnShowFileDialog;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnImportStart;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ Auto_DataImport/MainForm.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AutoItX3Lib;
+
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using DataTable = System.Data.DataTable;
+using KAutoHelper;
+using System.Diagnostics;
+
+namespace Auto_DataImport
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        DataTable data;
+
+        private void btnShowFileDialog_Click(object sender, EventArgs e)
+        {
+            string fname = "";
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "Excel File Dialog";
+            fdlg.InitialDirectory = @"c:\";
+            fdlg.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+            fdlg.FilterIndex = 2;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                fname = fdlg.FileName;
+                textBox1.Text = fdlg.FileName;
+            }
+            else
+            {
+                return;
+            }
+
+
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(fname);
+            Microsoft.Office.Interop.Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Microsoft.Office.Interop.Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int rowCount = xlRange.Rows.Count;
+            int colCount = xlRange.Columns.Count;
+
+
+            data = new DataTable();
+            data.Columns.Add(new DataColumn("STT", Type.GetType("System.Int32")));
+
+            // dt.Column = colCount;  
+            for (int j = 1; j <= colCount; j++)
+            {
+                if (xlRange.Cells[1, j] != null && xlRange.Cells[1, j].Value2 != null)
+                {
+                    data.Columns.Add(xlRange.Cells[1, j].Value2.ToString());
+                }
+            }
+            for (int i = 2; i < rowCount; i++)
+            {
+                DataRow row = data.NewRow();
+                row[0] = i - 1;
+                for (int j = 1; j <= colCount; j++)
+                {
+                    //write the value to the Grid  
+                    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                    {
+                        row[j] = xlRange.Cells[i, j].Value2.ToString();
+                    }
+                }
+                data.Rows.Add(row);
+            }
+
+            numericUpDown2.Value = data.Rows.Count;
+            dataGridView1.DataSource = data;
+
+            //cleanup  
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //rule of thumb for releasing com objects:  
+            //  never use two dots, all COM objects must be referenced and released individually  
+            //  ex: [somthing].[something].[something] is bad  
+
+            //release com objects to fully kill excel process from running in the background  
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release  
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release  
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+        }
+
+        private void btnImportStart_Click(object sender, EventArgs e)
+        {
+            if (data == null)
+            {
+                MessageBox.Show("Browse the Excel file to start importing data.");
+            }
+            else
+            {
+                var hWnd = IntPtr.Zero;
+                var temp = Process.GetProcessesByName("QuanLyPhatTu");
+                if (temp.Length == 0)
+                {
+                    MessageBox.Show("Đăng nhập rồi thử lại.");
+                    StartApp();
+                }
+                else
+                {
+                    hWnd = temp[0].MainWindowHandle;
+                    var clienthWnd = AutoControl.FindHandles(hWnd, "WindowsForms10.MDICLIENT.app.0.265601d_r9_ad1", null);
+                    AutoControl.BringToFront(hWnd);
+
+                    for (int i = 0; i < 6; i++)
+                    {
+                        AutoControl.SendKeyFocus(KeyCode.ESC);
+                    }
+                    var childhWnds = AutoControl.FindHandles(hWnd, "WindowsForms10.Window.8.app.0.265601d_r9_ad1", null);
+                    int buttonIndex = 0;
+
+
+                    System.Threading.Thread.Sleep(2000);
+
+                    AutoControl.SendClickOnPosition(childhWnds[buttonIndex], 120, 15);
+                    AutoControl.SendClickOnPosition(childhWnds[buttonIndex], 50, 50);
+                    for (int j = (int)numericUpDown1.Value - 1; j < data.Rows.Count && j < +(int)numericUpDown1.Value + (int)numericUpDown2.Value - 1; j++)
+                    {
+                        AutoControl.SendKeyFocus(KeyCode.F3);
+                        System.Threading.Thread.Sleep(1500);
+                        for (int i = 1; i < data.Columns.Count; i++)
+                        {
+                            if (i != 4)
+                            {
+
+                                if (!string.IsNullOrEmpty(data.Rows[j][i].ToString()) && i != 7)
+                                {
+                                    AutoControl.SendStringFocus(data.Rows[j][i].ToString());
+                                }
+                                System.Threading.Thread.Sleep(300);
+                                if (i == 7)
+                                {
+                                    if (data.Rows[j][i].ToString().Trim().ToLower() == "nữ")
+                                    {
+                                        AutoControl.SendKeyFocus(KeyCode.RIGHT);
+                                        System.Threading.Thread.Sleep(300);
+                                    }
+                                }
+                                AutoControl.SendKeyFocus(KeyCode.TAB);
+                                if (i == 19 || i == 23)
+                                {
+                                    AutoControl.SendKeyFocus(KeyCode.TAB);
+                                }
+                            }
+                        }
+                        AutoControl.SendKeyFocus(KeyCode.ESC);
+                        AutoControl.SendKeyFocus(KeyCode.RIGHT);
+                        AutoControl.SendKeyFocus(KeyCode.ENTER);
+                    }
+                }
+            }
+        }
+        private IntPtr StartApp()
+        {
+            Process p = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = @"C:\VTEK\QLPT\QuanLyPhatTu.exe";
+            startInfo.CreateNoWindow = false;
+            p.StartInfo = startInfo;
+            p.Start();
+            return p.Handle;
+        }
+        private void StartImport(IntPtr[] handles, int[] indexes, DataRow data)
+        {
+            for (int i = 0; i < indexes.Length; i++)
+            {
+                AutoControl.SendText(handles[indexes[i]], data[i].ToString());
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            decimal rs = numericUpDown1.Value;
+            if (rs < 1)
+            {
+                numericUpDown1.Value = 1;
+            }
+            else if (data != null && rs > data.Rows.Count)
+            {
+                numericUpDown1.Value = data.Rows.Count;
+            }
+        }
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            decimal rs = numericUpDown2.Value;
+            if (rs < 1)
+            {
+                numericUpDown2.Value = 1;
+            }
+            else if (data != null && rs + numericUpDown1.Value > data.Rows.Count + 1)
+            {
+                numericUpDown2.Value = data.Rows.Count - numericUpDown1.Value + 1;
+            }
+        }
+    }
+}
